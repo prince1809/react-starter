be186a305fc80c588ec3b1337c3ade67456f4ec0
prince <prince18pat@gmail.com>
2019-02-03 11:22:36 +0900

    create components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person'
+import  classes from './App.css';
+import Persons from '../components/Persons/Persons'
 
 class App extends Component {
 
@@ -44,49 +44,39 @@ class App extends Component {
 
   render() {
 
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+
+    let btnClass = '';
 
     if(this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-            click={() => this.deletePersonHandler(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
+          <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler} />
          </div>
       );
-      style.backgroundColor='red';
+
+      btnClass = classes.Red;
     }
 
-    let classes = [];
+    let assignedClasses= [];
 
     if (this.state.persons.length <= 2) {
-      classes.push('red')
+      assignedClasses.push(classes.red)
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working</p>
+        <p className={assignedClasses.join(' ')}>This is really working</p>
         <button 
-        style={style}
+        className={btnClass}
         onClick={this.togglePersonHandler}>Toggle Persons</button>
         {persons}
       </div>
