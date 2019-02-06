2e556567ba32be75e38858b1b54bc4237106e290
prince <prince18pat@gmail.com>
2019-02-07 00:20:28 +0900

    split components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Person.css';
+import WithClass from '../../../hoc/WithClass';
 
-const person = (props) => {
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
-};
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[Person.js] Inside constructor',props);
+    }
+    
+    componentWillMount(){
+        console.log('[Person.js] Inside ComponentWillMount');
+      }
+    
+    componentDidMount() {
+        console.log('[Person.js] Inside componentDidMount');
+    }
 
-export default person;
+    render () {
+        console.log('[Person.js] Inside render');
+        return (
+            <WithClass classes={classes.Person}>
+                <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </WithClass>
+        )
+    }
+}
+
+export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,56 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import  classes from './App.css';
-import Persons from '../components/Persons/Persons'
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
-class App extends Component {
+class App extends PureComponent {
 
-  state  = {
-    persons: [
-      {id: '1', name : 'Prince', age: 28},
-      {id: '2', name: 'John', age: 28},
-      {id: '3', name: 'Alex', age: 28}
-    ],
-    otherState: 'Some other vlaue',
-    showPersons: false
+  constructor(props) {
+    super(props)
+    console.log('[App.js] Inside constructor',props)
+    this.state = {
+      persons: [
+        {id: '1', name : 'Prince', age: 28},
+        {id: '2', name: 'John', age: 28},
+        {id: '3', name: 'Alex', age: 28}
+      ],
+      otherState: 'Some other value',
+      showPersons: false
+    }
   }
+
+  componentWillMount(){
+    console.log('[App.js] Inside ComponentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount')
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //   return true;
+  // }
+  
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] Inside componentDidUpdate');
+  }
+
+
+  // state  = {
+  //   persons: [
+  //     {id: '1', name : 'Prince', age: 28},
+  //     {id: '2', name: 'John', age: 28},
+  //     {id: '3', name: 'Alex', age: 28}
+  //   ],
+  //   otherState: 'Some other vlaue',
+  //   showPersons: false
+  // }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -44,42 +82,28 @@ class App extends Component {
 
   render() {
 
+    console.log('[App.js] Inside render');
+
     let persons = null;
 
-    let btnClass = '';
-
     if(this.state.showPersons) {
-      persons = (
-        <div>
+      persons =
           <Persons 
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler} />
-         </div>
-      );
-
-      btnClass = classes.Red;
-    }
-
-    let assignedClasses= [];
-
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red)
-    }
-
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold)
     }
 
     return (
-        <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working</p>
-        <button 
-        className={btnClass}
-        onClick={this.togglePersonHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <WithClass classes={classes.App}>
+          <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
+          <Cockpit 
+          appTitle={this.props.title}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonHandler} />
+          {persons}
+      </WithClass>
     );
   }
 }
